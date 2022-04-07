14d511f828bf758e0d29a9e1b340ae664afe514e
VishalBallal <vishalballal26@gmail.com>
2022-04-07 09:47:52 +0530

    Revision 2

--- python_project.py
+++ python_project.py
@@ -11,122 +11,144 @@ from sqlalchemy import create_engine
 # Create text file to record logs
 logging.basicConfig(
     filename="logfile.txt",
-    level=logging.WARNING,
+    level=logging.DEBUG,
     format="%(asctime)s:%(levelname)s:%(message)s")
 
 
-# 1. Get Data from API
-
-# 1.1 get data in json using provided API
-response = requests.get(r"https://jsonplaceholder.typicode.com/posts")
-
-# 1.2 check response of given API
-logging.warning(f"API responded with response number: {response.status_code}")
-print()
-
-# 1.3 check content of API in bytes
-# print(response.content)
-
-# 1.4 check content of API in unicode
-# print(response.text)
-
-
-# 2. Create Table in Database
-
-# 2.1 Create Connection between Python and Database
-db_username = os.getenv("user")
-db_password = os.getenv("password")
-db_host = os.getenv("host")
-db_port = os.getenv("port")
-db_database = os.getenv("database")
-
 try:
-    connection = psycopg2.connect(
-        user=db_username,
-        password=db_password,
-        host=db_host,
-        port=db_port,
-        database=db_database
-    )
-
-    cursor = connection.cursor()
-    logging.warning("Python and Database Connection Established Successfully")
-    print()
+    # get data in json using provided API
+    response1 = requests.get(r"https://jsonplaceholder.typicode.com/posts")
 
 except Exception as e:
-    print("Exception Occurs due to: ", e)
+    # check response of given API
+    print("Exception occurs due to", e)
 
-# 2.2 create table in database
-cursor.execute("""drop table if exists assign1""")
-connection.commit()
+else:
+    logging.debug(f"API responded with response: {response1.status_code}")
+    # check content of API in bytes
+    # print(response1.content)
 
-cursor.execute(
-    """create table assign1
-                 (userid           int,
-                  id               int,
-                  title   varchar(255),
-                  body    varchar(255),
-                  created         date,
-                  updated         date,
-                  deleted         date)
-                  """
-)
+    # check content of API in unicode
+    # print(response1.text)
 
-connection.commit()
-logging.warning("Table Created in Database")
-cursor.close()
+    # 2. Create Table in Database
 
+    # Create Connection between Python and Database
+    db_username = os.getenv("user")
+    db_password = os.getenv("password")
+    db_host = os.getenv("host")
+    db_port = os.getenv("port")
+    db_database = os.getenv("database")
 
-# 3. Filter API data for userId=2 and save the data in csv format
+    try:
+        connection = psycopg2.connect(
+            user=db_username,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+            database=db_database,
+        )
 
-# 3.1 filter data for userId = 2 from API and load it in json format
-response = requests.get(r"https://jsonplaceholder.typicode.com/posts?userId=2")
-filtered_data = json.loads(response.text)
+        cursor = connection.cursor()
+        logging.debug(
+            "Python and Database Connection Established Successfully"
+        )
+        print()
 
-# 3.2 read filtered data in Pandas DataFrame and Preprocess if required
-df = pd.DataFrame(filtered_data)
-print("API holds below data for userid = 2")
-print(df.head())
-print()
+    except Exception as e:
+        print("Exception Occurs due to: ", e)
 
-# 3.3 check column names
-# print(df.columns)
+    else:
+        # create table in database
+        cursor.execute("""drop table if exists assign1""")
+        connection.commit()
 
-# 3.4 rename column names
-df.columns = [i.lower() for i in df.columns]
-# print(df.columns)
+        cursor.execute(
+            """create table assign1
+                        (userid           int,
+                        id               int,
+                        title   varchar(255),
+                        body    varchar(255),
+                        created         date,
+                        updated         date,
+                        deleted         date)
+                        """
+        )
 
-# 3.5 add created and updated column in dataframe
-df["created"] = df.apply(lambda _: "", axis=1)
-df["updated"] = df.apply(lambda _: "", axis=1)
+        connection.commit()
+        logging.debug("Table Created in Database")
+        cursor.close()
 
-# 3.6 add current date in created and updated column
-df["created"] = df["created"].apply(lambda x: dt.date.today())
-df["updated"] = df["updated"].apply(lambda x: dt.date.today())
-print("Preprocessed Data")
-print(df.head())
-print()
+        # 3. Filter API data for userId=2 and save the data in csv format
+        try:
+            # get data in json using provided API
+            response2 = requests.get(
+                r"https://jsonplaceholder.typicode.com/posts?userId=2"
+            )
 
+        except Exception as f:
+            # check response of given API
+            print("Exception occurs due to", f)
 
-# 4. save dataframe in csv format
+        else:
+            logging.debug(
+                f"Response of filtered API:{response2.status_code}"
+            )
 
-df.to_csv("userid2.csv", index=False)
-logging.warning("preprocessed data stored in csv file")
+            # filter data for userId = 2 from API and load it in json format
+            filtered_data = json.loads(response2.text)
 
+            # read filtered data in Pandas DataFrame and Preprocess if required
+            df = pd.DataFrame(filtered_data)
+            print("API holds below data for userid = 2")
+            print(df.head())
+            print()
 
-# 5. Save Preprocessed DataFrame to Database
+            # check column names
+            # print(df.columns)
 
-# 5.1 create connection between python and database using sqlalchemy
-engine = create_engine("postgresql://" + db_username + ":" + db_password +
-                       "@" + db_host + ":" + db_port + "/" + db_database)
+            # rename column names
+            df.columns = [i.lower() for i in df.columns]
+            # print(df.columns)
 
-# 5.2 upload dataframe to database
-df.to_sql("assign1", con=engine, if_exists="append", index=False)
-logging.warning("Preprocessed Data uploaded to Database")
+            # add created and updated column in dataframe
+            df["created"] = df.apply(lambda _: "", axis=1)
+            df["updated"] = df.apply(lambda _: "", axis=1)
 
+            # add current date in created and updated column
+            df["created"] = df["created"].apply(lambda x: dt.date.today())
+            df["updated"] = df["updated"].apply(lambda x: dt.date.today())
+            print("Preprocessed Data")
+            print(df.head())
+            print()
 
-# 6. Read data from database
+            # 4. save dataframe in csv format
 
-db_df = pd.read_sql("select * from assign1", engine)
-print("Database Data")
-print(db_df)
+            df.to_csv("userid2.csv", index=False)
+            logging.debug("preprocessed data stored in csv file")
+
+            # 5. Save Preprocessed DataFrame to Database
+
+            # create connection between python and database using sqlalchemy
+            engine = create_engine(
+                "postgresql://"
+                + db_username
+                + ":"
+                + db_password
+                + "@"
+                + db_host
+                + ":"
+                + db_port
+                + "/"
+                + db_database
+            )
+
+            # upload dataframe to database
+            df.to_sql("assign1", con=engine, if_exists="append", index=False)
+            logging.debug("Preprocessed Data uploaded to Database")
+
+            # 6. Read data from database
+
+            db_df = pd.read_sql("select * from assign1", engine)
+            print("Database Data")
+            print(db_df)
